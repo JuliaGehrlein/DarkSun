a8eb94017c2ed71c4dbb615ad1065a5d724b3b97
JuliaGehrlein <45388518+JuliaGehrlein@users.noreply.github.com>
2019-01-14 14:45:00 +0100

    Add files via upload

--- sun_test.cpp
+++ sun_test.cpp
@@ -25,7 +25,7 @@ int main()
 
   //definition state (number flavours, neutrino or antineutrino)
   unsigned int Nneutrinos=3;
-  nuSQUIDS nus_sun(Nneutrinos,neutrino);
+  nuSQUIDS nus_evo(Nneutrinos,neutrino);
 
   // mixing angles
   double th12=0.563942;
@@ -36,14 +36,14 @@ int main()
   double delta=0.0;
 
   // Set neutrino mixing angles
-  nus_sun.Set_MixingAngle(0,1,th12);
-  nus_sun.Set_MixingAngle(0,2,th13);
-  nus_sun.Set_MixingAngle(1,2,th23);
+  nus_evo.Set_MixingAngle(0,1,th12);
+  nus_evo.Set_MixingAngle(0,2,th13);
+  nus_evo.Set_MixingAngle(1,2,th23);
   // square mass differences
-  nus_sun.Set_SquareMassDifference(1,dm21);
-  nus_sun.Set_SquareMassDifference(2,dm31);
+  nus_evo.Set_SquareMassDifference(1,dm21);
+  nus_evo.Set_SquareMassDifference(2,dm31);
   // CP phase
-  nus_sun.Set_CPPhase(0,2,delta);
+  nus_evo.Set_CPPhase(0,2,delta);
 
   // Define a Const object to handle units
   squids::Const units; // I love this
@@ -57,12 +57,15 @@ int main()
   std::shared_ptr<Sun> sun = std::make_shared<Sun>();
   std::shared_ptr<Sun::Track> track_sun = std::make_shared<Sun::Track>(0.0,sun->GetRadius());
 
-  nus_sun.Set_Body(sun);
-  nus_sun.Set_Track(track_sun);
+  nus_evo.Set_Body(sun);
+  nus_evo.Set_Track(track_sun);
 
+  double Emin=10*units.MeV;
+  double Emax=100*units.MeV;
+  for(double Einit=Emin; Einit<=Emax; Einit+=(Emax-Emin)/10){
   // set initial energy in GeV
-  double Einit=15*units.MeV;
-  nus_sun.Set_E(Einit);
+  //double Einit=15*units.MeV;
+  nus_evo.Set_E(Einit);
 
   //enter DM mass in GeV
   double massdm = 10;
@@ -83,73 +86,75 @@ int main()
   marray<double,1> ini_state({Nneutrinos},{fluxe.flux_pion(massdm,log10(Einit/massdm),channel),fluxmu.flux_pion(massdm,log10(Einit/massdm),channel),fluxtau.flux_pion(massdm,log10(Einit/massdm),channel)});
 
   // Actually sets the initial state
-  nus_sun.Set_initial_state(ini_state,flavor);
+  nus_evo.Set_initial_state(ini_state,flavor);
   cout << "In state Sun" << "\n";
-  for (double EE : nus_sun.GetERange()){
+  for (double EE : nus_evo.GetERange()){
     cout << EE/units.GeV << " ";
     for(int i = 0; i < Nneutrinos; i++){
-      cout << nus_sun.EvalFlavor(i) << " ";
+      cout << nus_evo.EvalFlavor(i) << " ";
     }
     cout << "\n";
   }
 
   //set precision
   //this affects the running time
-  nus_sun.Set_h_max( 200.0*units.km );
-  nus_sun.Set_rel_error(1.0e-12);
-  nus_sun.Set_abs_error(1.0e-12);
+  nus_evo.Set_h_max( 50.0*units.km );
+  nus_evo.Set_rel_error(1.0e-12);
+  nus_evo.Set_abs_error(1.0e-12);
 
   //evolution of the state
-  nus_sun.EvolveState();
+  nus_evo.EvolveState();
 
   // Output the result
   cout << "Out state Sun" << endl;
-  for (double EE : nus_sun.GetERange()){
+  for (double EE : nus_evo.GetERange()){
     cout << EE/units.GeV << " ";
     for(int i = 0; i < Nneutrinos; i++){
-      cout << nus_sun.EvalFlavor(i) << " ";
+      cout << nus_evo.EvalFlavor(i) << " ";
     }
     cout << "\n";
   };
 
-  //propagtion through vacuum
+  //propagation through vacuum
   std::shared_ptr<Vacuum> vacuum = std::make_shared<Vacuum>();
 
   //distance Sun-earth= 1 AU=149.598 *10^6 km, the distances varies by 1.6% over a year (149 597 870 700 m)
   double baseline_2 = 149597870.0*units.km;
   std::shared_ptr<Vacuum::Track> track_vac = std::make_shared<Vacuum::Track>(baseline_2);
-  nuSQUIDS nus_vac(Nneutrinos,neutrino);
 
-  nus_vac.Set_Body(vacuum);
-  nus_vac.Set_Track(track_vac);
-  nus_vac.Set_E(Einit);
+  //Redefine
+  nus_evo.Set_Body(vacuum);
+  nus_evo.Set_Track(track_vac);
 
-  // initial state is final state from the Sun
-  ini_state = {nus_sun.EvalFlavor(0),nus_sun.EvalFlavor(1),nus_sun.EvalFlavor(2)};
-
-  // Actually set the initial condition
-  nus_vac.Set_initial_state(ini_state,flavor);
+  // WHY IS THIS NECESSARY?????????
+  ini_state = {nus_evo.EvalFlavor(0),nus_evo.EvalFlavor(1),nus_evo.EvalFlavor(2)};
+  nus_evo.Set_initial_state(ini_state,flavor);
 
   // Print initial fluxes for propagation between Sun and Earth
   cout << "In state vacuum" << "\n";
-  for (double EE : nus_vac.GetERange()){
+  for (double EE : nus_evo.GetERange()){
     std::cout << EE/units.GeV << " ";
     for(int i = 0; i < Nneutrinos; i++){
-      std::cout << nus_vac.EvalFlavor(i) << " ";
+      std::cout << nus_evo.EvalFlavor(i) << " ";
     }
     std::cout << "\n";
   }
-  nus_vac.EvolveState();
+  nus_evo.EvolveState();
 
   // Print fluxes after propagation
   cout << "Out state vacuum" << "\n";
-  for (double EE : nus_vac.GetERange()){
+  for (double EE : nus_evo.GetERange()){
     std::cout << EE/units.GeV << " ";
     for(int i = 0; i < Nneutrinos; i++){
-      std::cout << nus_vac.EvalFlavor(i) << " ";
+      std::cout << nus_evo.EvalFlavor(i) << " ";
     }
     std::cout << "\n";
   }
+
+  writeout at1AU;
+  string name1AU="1AU_flux";
+  at1AU.writetxtfile(nus_evo,name1AU,Einit,Nneutrinos);
+  at1AU.writehdf5file(nus_evo,name1AU);
 
   //propagation through the earth
   //set the zenith angle
@@ -157,55 +162,53 @@ int main()
 
   std::shared_ptr<EarthAtm> earth_atm = std::make_shared<EarthAtm>();
   std::shared_ptr<EarthAtm::Track> earth_atm_track = std::make_shared<EarthAtm::Track>(phi);
-  cout << earth_atm_track->GetBaseline()/units.km << " ...? \n";
-  nuSQUIDS nus_earth(Nneutrinos,neutrino);
-  nus_earth.Set_Body(earth_atm);
-  nus_earth.Set_Track(earth_atm_track);
+  //  cout << earth_atm_track->GetBaseline()/units.km << " ...? \n";
 
-  // setup integration settings
-  nus_earth.Set_rel_error(1.0e-12);
-  nus_earth.Set_abs_error(1.0e-12);
+  nus_evo.Set_Body(earth_atm);
+  nus_evo.Set_Track(earth_atm_track);
 
-  // Set energy
-  nus_earth.Set_E(Einit);
-  // Define initial state
-  ini_state = {nus_vac.EvalFlavor(0),nus_vac.EvalFlavor(1),nus_vac.EvalFlavor(2)};
-  // Actually define initial state
-  nus_earth.Set_initial_state(ini_state,flavor);
+  // AND WHY IS THIS NECESSARY???????
+  ini_state = {nus_evo.EvalFlavor(0),nus_evo.EvalFlavor(1),nus_evo.EvalFlavor(2)};
+  nus_evo.Set_initial_state(ini_state,flavor);
 
   // Print the flux that enters the Earth
   cout << "In state earth" << "\n";
-  for (double EE : nus_earth.GetERange()){
+  for (double EE : nus_evo.GetERange()){
     cout << EE/units.GeV << " ";
     for(int i = 0; i < Nneutrinos; i++){
-      cout << nus_earth.EvalFlavor(i) << " ";
+      cout << nus_evo.EvalFlavor(i) << " ";
     }
     cout << "\n";
   }
 
   //Perform evolution through the Earth
-  nus_earth.EvolveState();
+  nus_evo.EvolveState();
 
   // Print the flux that leaves the Earth
   cout << "Out state earth" << "\n";
-  for (double EE : nus_earth.GetERange()){
+  for (double EE : nus_evo.GetERange()){
     cout << EE/units.GeV << " ";
     for(int i = 0; i < Nneutrinos; i++){
-      cout << nus_earth.EvalFlavor(i) << " ";
+      cout << nus_evo.EvalFlavor(i) << " ";
     }
     cout << "\n";
   }
 
-
+  /*
   //write hdf5 file with final flux
-  nus_earth.WriteStateHDF5("flux_final_dm.hdf5");
+  nus_evo.WriteStateHDF5("flux_final_dm.hdf5");
 
   //Writing to the file!
   file << Einit/units.GeV;
   for(int fl=0; fl<Nneutrinos; fl++){
-    file<<" "<< nus_earth.EvalFlavor(fl)<<" ";
+    file<<" "<< nus_evo.EvalFlavor(fl)<<" ";
   }
   file << "\n";
-
+  */
+  writeout final;
+  string name="final_flux";
+  final.writetxtfile(nus_evo,name,Einit,Nneutrinos);
+  final.writehdf5file(nus_evo,name);
+  }
   return 0;
 }
